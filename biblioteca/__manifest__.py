946835f1207042a75262a489b05cb99b07e4883f
Diego Calderon <dmcalderonc97@gmail.com>
2025-10-02 10:09:03 -0500

    odoo visual

--- biblioteca/__manifest__.py
+++ biblioteca/__manifest__.py
@@ -22,13 +22,16 @@ Long description of module's purpose
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+         'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
+        #'views/templates.xml',
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    #'demo': [
+    #    'demo/demo.xml',
+    #],
+    'aplication': True,
+    'licence': 'AGPL-3'
+        
 }
 
