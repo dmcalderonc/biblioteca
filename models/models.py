6f68f0b7c82e253e23c7b3e1a9ff284982a2cdff
dmcalderonc <dmcalderonc97@gmail.com>
2025-10-11 17:47:44 -0500

    modificación del menú libro, y adicion del menú usuarios

--- models/models.py
+++ models/models.py
@@ -7,12 +7,17 @@ class Biblioteca(models.Model):
     _name = 'biblioteca.libro'
     _description = 'biblioteca.biblioteca'
 
-    firstname = fields.Char(string='Nombre Libro')
+    firstname = fields.Char(string='Titulo')
     author = fields.Many2one('biblioteca.author', string='Author del libro')
-    value = fields.Integer(string='Número de ejemplares')
-    value2 = fields.Float(compute="_value_pc", store=True, string='Costo')
+    isbn = fields.Char(string='ISBN')
+    genero = fields.Char(string='Género')
+    fecha = fields.Char(string='Año de publicación')
     description = fields.Text(string='Resumen del libro')
-    
+    value = fields.Integer(string='Número de ejemplares')
+    estado= fields.Char(string='Estado')
+    value2 = fields.Float(compute="_value_pc", store=True, string='Costo')
+    ubicacion = fields.Char(string="Ubicación del libro")
+
     @api.depends('value')
     def _value_pc(self):
         for record in self:
@@ -22,11 +27,20 @@ class BibliotecaAutor(models.Model):
     _name= 'biblioteca.author'
     _description = 'biblioteca.author'
     _rec_name= 'firstname'
-    firstname = fields.Char()
-    lastname = fields.Char()
+    firstname = fields.Char(string='Nombre')
+    lastname = fields.Char(string='Apellido')
     
     @api.depends('firstname','lastname')
     def _compute_display_name(self):
         for record in self:
             record.display_name= f"{record.firstname}{" "}{record.lastname}"
+
+class BibliotecaUsuario(models.Model):
+    _name= 'biblioteca.usuario'
+    _description= 'biblioteca.usuario'
+
+    firstname= fields.Char(string='Nombre')
+    lastname = fields.Char(string='Apellido')
+    cedula = fields.Integer(string='Cédula')
+    telefono= fields.Integer(string='Telefono')
         
