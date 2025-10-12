427b217755d2f188e36a39f380a23afa947ce322
dmcalderonc <dmcalderonc97@gmail.com>
2025-10-12 11:27:18 -0500

    adicion de los editoriales, generos, ubicacion, personal, prestamos y multas

--- models/models.py
+++ models/models.py
@@ -6,18 +6,19 @@ from odoo import models, fields, api
 class Biblioteca(models.Model):
     _name = 'biblioteca.libro'
     _description = 'biblioteca.biblioteca'
+    _rec_name= 'firstname'
 
     firstname = fields.Char(string='Titulo')
     author = fields.Many2one('biblioteca.author', string='Author del libro')
     isbn = fields.Char(string='ISBN')
-    genero = fields.Char(string='Género')
+    genero = fields.Many2one('biblioteca.genero', string='Género')
     fecha = fields.Char(string='Año de publicación')
     description = fields.Text(string='Resumen del libro')
     editorial = fields.Many2one('biblioteca.editorial', string='Editorial')
     value = fields.Integer(string='Número de ejemplares')
     estado= fields.Char(string='Estado')
     value2 = fields.Float(compute="_value_pc", store=True, string='Costo')
-    ubicacion = fields.Char(string="Ubicación del libro")
+    ubicacion = fields.Many2one('biblioteca.ubicacion', string="Ubicación del libro")
 
     @api.depends('value')
     def _value_pc(self):
@@ -30,6 +31,7 @@ class BibliotecaAutor(models.Model):
     _rec_name= 'firstname'
     firstname = fields.Char(string='Nombre')
     lastname = fields.Char(string='Apellido')
+    alias= fields.Char(string='Pseudonimo')
     
     @api.depends('firstname','lastname')
     def _compute_display_name(self):
@@ -68,4 +70,47 @@ class BibliotecaPersonal(models.Model):
     cargo= fields.Char(string='Cargo')
     usuario= fields.Char(string='Usuario')
     clave= fields.Char(string='Clave')
+
+class BibliotecaGeneros(models.Model):
+    _name= 'biblioteca.genero'
+    _description= 'biblioteca.genero'
+    _rec_name='genero'
+    genero= fields.Char(string='Género')
+    descripcion=fields.Char(string='Descripción')
+    @api.depends('genero')
+    def _compute_display_name(self):
+        for record in self:
+            record.display_name= f"{record.genero}"
+
+
+class BibliotecaPrestamos(models.Model):
+    _name= 'biblioteca.prestamo'
+    _description= 'biblioteca.prestamo'
+    
+    usuario= fields.Char(string='Usuario')
+    libro=fields.Many2one('biblioteca.libro', string='Titulo del libro')
+    fecha_prestamo= fields.Date(string='Fecha de prestamo')
+    fecha_devolucion= fields.Date(string='Fecha de devolución')
+    estado= fields.Selection(selection=[('disponible', 'Disponible'),
+                                        ('devuelto', 'Devuelto'),
+                                        ('encurso', 'En curso'),
+                                        ('retrasado', 'Retrasado'),],string='Estado')
+    
+class BibliotecaMultas(models.Model):
+    _name= 'biblioteca.multa'
+    _description= 'biblioteca.multa'
+    usuario= fields.Char(string='Usuario')
+    monto= fields.Float(string='Monto a pagar')
+    motivo= fields.Selection(selection=[('retraso', 'Retraso'),
+                                        ('daño', 'Daño'),
+                                        ('perdida','Perdida')],string='Causa de la multa')
+    pago= fields.Selection(selection=[('pendiente', 'Pendiente'),
+                                        ('saldada', 'Saldada')],string='Pago  de la multa')
+    
+class BibliotecaUbicacion(models.Model):
+    _name= 'biblioteca.ubicacion'
+    _description= 'biblioteca.ubicacion'
+    _rec_name= 'ubicacion'
+    libro= fields.Many2one('biblioteca.libro', string='Titulo del libro')
+    ubicacion= fields.Char(string='Ubicación')
 
